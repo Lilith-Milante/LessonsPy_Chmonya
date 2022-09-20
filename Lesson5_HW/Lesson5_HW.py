c4555d17667a116638162111d0cf6bcfc6e9df04
Wirral_Riddler <anestshchuch@gmail.com>
2022-09-21 02:07:37 +0300

    create project and add solutions

--- Lesson5_HW/Lesson5_HW.py
+++ Lesson5_HW/Lesson5_HW.py
@@ -1,5 +1,66 @@
 # 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
+map = [1, 2, 3, 4, 5, 6, 7, 8, 9] #создаём игровое поле
+
+def print_map():
+    print(map[0], end = '|')
+    print(map[1], end = '|')
+    print(map[2])
+    print('-----')
+    print(map[3], end = '|')
+    print(map[4], end = '|')
+    print(map[5])
+    print('-----')
+    print(map[6], end = '|')
+    print(map[7], end = '|')
+    print(map[8])
+
+comb = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,7], [1,5,9], [2,4,6]] # это победные комбинации, [,,] - 1 элемент
+
+def enter_pl(enter, symbol): # функция для ввода символа и подмены
+    index = map.index(enter)
+    map[index] = symbol
+
+def result_check():
+    win = '' #запишем сюда победителя
+    for i in comb:
+        if map[i[0]] == 'X' and map[i[1]] == 'X' and map[i[2]] == 'X': # в каждом элементе comb проверяем, все ли элементы [,,] Х или 0
+            win = 'X'
+        if map[i[0]] == '0' and map[i[1]] == '0' and map[i[2]] == '0':
+            win = '0'
+    return win
+
+player1_name = input("Enter the name of first player: ")
+player2_name = input("Enter the name of second player: ")
+
+game_over = False
+player1 = True
+
+while game_over == False:
+    print_map()
+
+    if player1 == True:
+        symbol = 'X'
+        enter = int(input(f'{player1_name}, where do you put X? '))
+    else:
+        symbol = '0'
+        enter = int(input(f'{player2_name}, where do you put 0? '))
+    enter_pl(enter, symbol) # ход
+
+    win = result_check()
+
+    if win != '':
+        game_over = True
+    else:
+        game_over = False
+    player1 = not(player1)
+
+print_map()
+print('Victory', win)
+
+
+exit()
+
 str = input('Enter str: ').replace('абв', '').split(' ')
 result = ' '.join(list(filter(lambda x: x != '', str)))
 print(result)
