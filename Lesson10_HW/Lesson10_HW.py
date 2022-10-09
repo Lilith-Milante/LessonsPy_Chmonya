803ee173721e8ce2aac03e33ae26af6fe68b6552
Wirral_Riddler <anestshchuch@gmail.com>
2022-10-09 16:28:44 +0300

    create project and add solutions

--- Lesson10_HW/Lesson10_HW.py
+++ Lesson10_HW/Lesson10_HW.py
@@ -1,46 +1,6 @@
 import time
 
-# 1. Создать класс TrafficLight (светофор):
-# определить у него один атрибут color (цвет) и метод running (запуск);
-# атрибут реализовать как приватный;
-# в рамках метода реализовать переключение светофора в режимы: красный, жёлтый,
-# зелёный;
-# продолжительность первого состояния (красный) составляет 7 секунд, второго
-# (жёлтый) — 2 секунды, третьего (зелёный) — на ваше усмотрение;
-# переключение между режимами должно осуществляться только в указанном порядке
-# (красный, жёлтый, зелёный);
-# проверить работу примера, создав экземпляр и вызвав описанный метод.
-
-class TrafficLight():
-    __color = 0
-
-    def __init__(self, red, yellow, green):
-        self.mode = 0
-        self.colors = [red, yellow, green]
-        
-    def running (self):
-        
-        while self.mode < 3:
-            if self.mode == 0:
-                TrafficLight.__color = self.colors[0]
-                print('red')
-                time.sleep(7)
-                self.mode += 1
-                
-            elif self.mode == 1:
-                TrafficLight.__color = self.colors[1]
-                print('yellow')
-                time.sleep(2)
-                self.mode += 1
-                
-            elif self.mode == 2:
-                TrafficLight.__color = self.colors[2]
-                print('green')
-                time.sleep(3)
-            self.mode +=1
-
-# 2 var           
-exs = TrafficLight('red', 'yellow', 'green')
+# 1. Создать класс TrafficLight (светофор)
 
 class TrafficLight():
     __color = 0
@@ -62,16 +22,10 @@ class TrafficLight():
         print(TrafficLight.__color)
         time.sleep(3)
 
-exs = TrafficLight('red', 'yellow', 'green')
+exs = TrafficLight('Red', 'Yellow', 'Green')
+print(exs.running())
 
-# 2. Реализовать класс Road (дорога).
-# определить атрибуты: length (длина), width (ширина);
-# значения атрибутов должны передаваться при создании экземпляра класса;
-# атрибуты сделать защищёнными;
-# определить метод расчёта массы асфальта, необходимого для покрытия всей дороги;
-# использовать формулу: длина*ширина*масса асфальта для покрытия одного кв. метра
-# дороги асфальтом, толщиной в 1 см*число см толщины полотна;
-# проверить работу метода.
+# 2. Реализовать класс Road (дорога)
 
 class Road:
     def __init__(self, _length, _width, _weight, _thickness):
@@ -85,17 +39,12 @@ class Road:
         return self._length * self._width * self._weight * self._thickness / 1000
         
 r = Road(20, 5000, 25, 5)
-print(int(r.get_mass()))
+r1 = Road(10, 10, 36, 7)
 
-# 3. Реализовать базовый класс Worker (работник):
-# определить атрибуты: name, surname, position (должность), income (доход);
-# последний атрибут должен быть защищённым и ссылаться на словарь, содержащий
-# элементы «оклад» и «премия», например, {"wage": wage, "bonus": bonus};
-# создать класс Position (должность) на базе класса Worker;
-# в классе Position реализовать методы получения полного имени сотрудника
-# (get_full_name) и дохода с учётом премии (get_total_income);
-# проверить работу примера на реальных данных: создать экземпляры класса Position,
-# передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+print(int(r.get_mass()))
+print(int(r1.get_mass()))
+
+# 3. Реализовать базовый класс Worker (работник)
 
 class Worker:
     
@@ -122,16 +71,60 @@ print(atr.get_total_income())
 print(atr1.get_full_name())
 print(atr1.get_total_income())
 
-
 # Реализуйте базовый класс Car:
-# у класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А
-# также методы: go, stop, turn(direction), которые должны сообщать, что машина
-# поехала, остановилась, повернула (куда);
-# опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar;
-# добавьте в базовый класс метод show_speed, который должен показывать текущую
-# скорость автомобиля;
-# для классов TownCar и WorkCar переопределите метод show_speed. При значении
-# скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о
-# превышении скорости.
 
+class Car:
 
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def get_name_color(self):
+        return f'{self.name} {self.color}'
+
+    def show_speed(self):
+        return self.speed
+
+    def go (self):
+        print('An auto grove')
+
+    def stop (self):
+        print('An auto stopped')
+
+    # def turn_direction(self):
+
+class TownCar(Car):
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Over speed!')
+        else:
+            return self.speed
+
+# class SportCar(Car):
+
+class WorkCar(Car):
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Over speed!')
+        else:
+            return self.speed
+
+class PoliceCar(Car):
+
+    def police_car(self):
+        if self.is_police: print('Give me your docs')
+        else: print('Drive calmly')
+
+a = TownCar(50, 'Ford', 'Violet', False)
+print(a.show_speed())
+print(a.get_name_color())
+
+a = PoliceCar(50, 'Audi', 'Red', True)
+a = PoliceCar(50, 'Audi', 'Red', False)
+print(a.get_name_color())
+print(a.show_speed())
+print(a.police_car())
